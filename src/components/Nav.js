ee6d36ce17073aa7f5d3f06a807f2078df9f8b48
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2019-09-21 21:21:45 +0700

    creating some components

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css';
+
+function Nav() {
+  return (
+    <nav>
+        <h4>Logo</h4>
+        <ul className="nav-links">
+            <li>About</li>
+            <li>Shop</li>
+        </ul>
+    </nav>
+  );
+}
+
+export default Nav;
